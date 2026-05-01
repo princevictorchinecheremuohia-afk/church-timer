1c96ab57c6a2ce3493ff4694ac5f1430f7974bc4
Prince Victor <princevictorchinecheremuohia@gmail.com>
2026-05-01 08:08:09 +0100

    church timer project

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,132 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [inputMinutes, setInputMinutes] = useState("0")
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [presentationMode, setPresentationMode] = useState(false)
+
+  useEffect ( () => {
+    let timer: NodeJS.Timeout
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prev) => {
+          if (prev <= 0) {
+            clearInterval(timer);
+            return 0;
+          }
+          return prev -1;
+        })
+      }, 1000);
+    }
+
+    return () => clearInterval(timer)
+
+  }, [isRunning]);
+
+  // Spacebar control
+  useEffect (() => {
+    const handlekey = (e: KeyboardEvent) => {
+      if (e.code === "Space") {
+        setIsRunning((prev) => !prev);
+      }
+    };
+
+    window.addEventListener("keydown", handlekey);
+    return () => window.removeEventListener("keydown", handlekey);
+  }, []);
+
+  // Local Storage
+  useEffect (() => {
+    const saved = localStorage.getItem("church-timer");
+    if (saved) {
+      setTime(Number(saved));
+    }
+  }, []);
+
+  useEffect (() => {
+    localStorage.setItem("church-timer", String(time));
+  }, [time]);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="h-screen w-full overflow-hidden bg-black text-white flex flex-col items-center justify-center">
+      {!presentationMode && (
+        <>
+          {/* Church Timer Title */}
+            <h1 className="text-5xl md:text-6xl mb-6 font-semibold tracking-wide">
+              Church Timer
+            </h1>
+        </>
+      )}
+
+      {/* Time Display */}
+      <h2 className="text-8xl md:text-9xl font-bold">
+        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </h2>
+
+      {/* Back Button */}
+      {presentationMode && (
+        <>
+          <button onClick={() => setPresentationMode(false)} className="absolute top-5 right-5 opacity-20 px-4 py-2 bg-white/20 text-white rounded-md text-sm hover:opacity-100 transition cursor-pointer">
+            Exit
+          </button>
+        </>
+      )}
+
+      {!presentationMode && (
+        <>
+          {/* Input Button */}
+      <div className="flex items-center gap-3 mb-6 mt-3">
+        <input type="number" placeholder="0" value={inputMinutes} onFocus={() => setInputMinutes("")} onChange={(e) => {const value = e.target.value; const cleaned = value.replace(/^0+/, ""); setInputMinutes(cleaned);}} className="px-4 py-2 bg-white text-black rounded-md w-24 outline-none" />
+
+        <button onClick={() => {
+          setTime(Number(inputMinutes) * 60);
+          setIsRunning(false)
+        }} className="px-4 py-2 bg-blue-500 rounded-md cursor-pointer hover:bg-blue-600 transition">
+          Set Time
+        </button>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex gap-4 px-1">
+        <button 
+          onClick={() => setIsRunning(true)} 
+          className="px-6 py-3 bg-green-600 rounded-xl text-xl cursor-pointer hover:bg-green-700 transition">
+            start
+        </button>
+
+        <button 
+          onClick={() => setIsRunning(false)} 
+          className="px-6 py-3 bg-yellow-500 rounded-xl text-xl cursor-pointer hover:bg-yellow-600 transition">
+            Pause
+        </button>
+
+        <button 
+          onClick={() => {setIsRunning(false); setTime(0)}} 
+          className="px-6 py-3 bg-red-600 rounded-xl text-xl cursor-pointer hover:bg-red-700 transition">
+            Reset
+        </button>
+
+        {/* Full Screen Button */}
+        <button 
+        onClick={() => document.documentElement.requestFullscreen()}
+        className="px-6 py-3 bg-blue-600 rounded-xl text-xl cursor-pointer hover:bg-blue-700 transition">
+          Full Screen
+        </button>
+      </div>
+
+      {/* Toggle Presentation Button */}
+      <button onClick={() => setPresentationMode((prev) => !prev)} className="px-6 py-3 bg-purple-600 rounded-xl text-xl mt-6 cursor-pointer hover:bg-purple-700 transition">
+        Toggle Presentation
+      </button>
+        </>
+      )}
     </div>
-  );
+  )
 }
